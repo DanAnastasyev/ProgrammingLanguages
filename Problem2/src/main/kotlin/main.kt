32d05d97e4ed9a895881b86184c08274821876e4
DanAnastasyev <dan.anastasyev@gmail.com>
2017-11-12 17:51:26 +0300

    Обработка ссылок на другие ячейки

--- Problem2/src/main/kotlin/main.kt
+++ Problem2/src/main/kotlin/main.kt
@@ -5,9 +5,15 @@ import org.w3c.dom.events.KeyboardEvent
 import org.w3c.dom.get
 import kotlin.browser.document
 
-class Sheet {
+
+class Sheet : Table {
     //val rawTable = Array<Array<String>>(20, { Array('Z' - 'A' + 1, {""}) })
-    val parser = ExpressionParser()
+    private val parser = ExpressionParser(this)
+
+    override fun getCellText(rowInd : Int, colInd : Char) : String {
+        val cell = document.getElementById("${rowInd}_$colInd") as HTMLElement
+        return cell.innerText
+    }
 
     fun buildTable() {
         val body = document.getElementsByTagName("body")[0]
@@ -26,28 +32,8 @@ class Sheet {
             headerCell.id = "$rowInd"
             row.appendChild(headerCell)
 
-            for (colInd in 'A'..'Z') {
-                val cell : HTMLElement
-                if (rowInd == 0) {
-                    cell = document.createElement("th") as HTMLElement
-                    cell.innerText = colInd.toString()
-                } else {
-                    cell = document.createElement("td") as HTMLElement
-                    cell.contentEditable = "true"
-                    cell.addEventListener("keypress", { e : Event ->
-                        e as KeyboardEvent
-                        if (e.keyCode == 13) {
-                            if (cell.innerText.startsWith('=')) {
-                                cell.innerText = parser.parse(cell.innerText.substring(1)).toString()
-                            }
-                            e.preventDefault()
-                        }
-                    })
-                }
-                cell.style.width = "100px"
-                cell.id = "${rowInd}_$colInd"
-                row.appendChild(cell)
-            }
+            ('A'..'Z').map { buildCell(rowInd, it) }
+                      .forEach { row.appendChild(it) }
 
             tblBody.appendChild(row)
         }
@@ -55,6 +41,33 @@ class Sheet {
         tbl.appendChild(tblBody)
         body?.appendChild(tbl)
         tbl.setAttribute("border", "1")
+    }
+
+    private fun buildCell(rowInd : Int, colInd : Char): HTMLElement {
+        val cell : HTMLElement
+        if (rowInd == 0) {
+            cell = document.createElement("th") as HTMLElement
+            cell.innerText = colInd.toString()
+        } else {
+            cell = document.createElement("td") as HTMLElement
+            cell.contentEditable = "true"
+            cell.addEventListener("keypress", { e: Event ->
+                e as KeyboardEvent
+                if (e.keyCode == 13) {
+                    if (cell.innerText.startsWith('=')) {
+                        try {
+                            cell.innerText = parser.parse(cell.innerText.substring(1)).toString()
+                        } catch (ex : Throwable) {
+                            cell.innerText = ex.toString()
+                        }
+                    }
+                    e.preventDefault()
+                }
+            })
+        }
+        cell.style.width = "100px"
+        cell.id = "${rowInd}_$colInd"
+        return cell
     }
 }
 
--- Problem2/src/main/kotlin/parser.kt
+++ Problem2/src/main/kotlin/parser.kt
@@ -14,31 +14,60 @@ fun Char.isDigit() : Boolean = this in '0'..'9'
 
 fun Char.isLetter() : Boolean = this in 'A'..'Z'
 
-class Token(val text: String) {
-    override fun toString() = text
+interface Token
+
+class OperatorToken(val operation : Char) : Token /*{
+    override fun toString(): String {
+        return operation.toString()
+    }
+}*/
+
+class NumberToken(text : String) : Token {
+    val value = text.toDouble()
+
+//    override fun toString(): String {
+//        return value.toString()
+//    }
+}
+
+class FunctionToken(val text : String) : Token /*{
+    override fun toString(): String {
+        return text
+    }
+}*/
+
+class CellToken(private val text : String) : Token {
+    val rowIndex = text.substring(1).toInt()
+    val columnIndex = text[0]
+
+//    override fun toString(): String {
+//        return text
+//    }
 }
 
 enum class SymbolType {
     DIGIT, LETTER, OPERATOR, WHITESPACE, UNDEFINED
 }
 
+val unaryMinus = OperatorToken('—')
+val openParenthesis = OperatorToken('(')
+val closeParenthesis = OperatorToken(')')
+
 class ExpressionTokenizer(private val line : String, private val specialTokens : HashSet<String>) {
     private val operators = hashSetOf('+', '-', '*', '/', '(', ')')
     private val cellRegex = Regex("[A-Z][0-9]{1,2}")
     private val digitRegex = Regex("([0-9]+)|([0-9]*\\.[0-9]+)")
 
-    private fun isGoodToken(token : String): Boolean {
-        return specialTokens.contains(token) or digitRegex.matches(token) or cellRegex.matches(token);
-    }
-
     private suspend fun SequenceBuilder<Token>.handleLongToken(token : String) {
-        if (token.isNotBlank()) {
-            if (isGoodToken(token)) {
-                yield(Token(token))
-            } else {
-                throw Exception("Wrong token: " + token)
-            }
+        if (token.isBlank()) {
+            return
         }
+        yield(when {
+            specialTokens.contains(token) -> FunctionToken(token)
+            digitRegex.matches(token) -> NumberToken(token)
+            cellRegex.matches(token) -> CellToken(token)
+            else -> throw Exception("Wrong token: " + token)
+        })
     }
 
     fun getTokens(): Iterator<Token> = buildIterator {
@@ -57,9 +86,9 @@ class ExpressionTokenizer(private val line : String, private val specialTokens :
                 SymbolType.OPERATOR -> {
                     handleLongToken(token)
                     if (line[pos] == '-' && isUnaryMinusPossible) {
-                        yield(Token("—"))
+                        yield(OperatorToken('—'))
                     } else {
-                        yield(Token(line.substring(pos, pos + 1)))
+                        yield(OperatorToken(line[pos]))
                     }
                     token = ""
                     isUnaryMinusPossible = line[pos] != ')'
@@ -87,14 +116,14 @@ class Value(private val value : Double) : Expression {
     override fun eval() = value
 }
 
-class Operator(private val left : Expression, private val operation : String,
+class Operator(private val left : Expression, private val operation : Char,
                private val right : Expression) : Expression {
 
     override fun eval() : Double = when (operation) {
-        "+" -> left.eval() + right.eval()
-        "-" -> left.eval() - right.eval()
-        "*" -> left.eval() * right.eval()
-        "/" -> left.eval() / right.eval()
+        '+' -> left.eval() + right.eval()
+        '-' -> left.eval() - right.eval()
+        '*' -> left.eval() * right.eval()
+        '/' -> left.eval() / right.eval()
         else -> throw Exception("Wrong operation type")
     }
 }
@@ -110,50 +139,36 @@ class Function(private val function : String,
     }
 }
 
-class ExpressionParser {
+interface Table {
+    fun getCellText(rowInd : Int, colInd : Char) : String
+}
+
+class ExpressionParser(private val table : Table) {
     private val functionTokens = hashSetOf("ABS", "SIN", "LEN")
 
     fun parse(expr : String) : Double {
         val expressionStack = ArrayList<Expression>()
-        val operatorStack = ArrayList<String>()
+        val operatorStack = ArrayList<Token>()
 
         val tokenizer = ExpressionTokenizer(expr, functionTokens)
         for (token in tokenizer.getTokens()) {
-            when (token.text) {
-                "-", "+", "*", "/", in functionTokens -> {
-                    while (operatorStack.isNotEmpty()
-                            && getPrecedence(operatorStack.last()) >= getPrecedence(token.text)) {
-                        addOperator(operatorStack, expressionStack)
+            when (token) {
+                is OperatorToken -> handleOperatorToken(token, operatorStack, expressionStack)
+                is FunctionToken -> handleStandardOperationToken(token, operatorStack, expressionStack)
+                is NumberToken -> handleNumberToken(token, operatorStack, expressionStack)
+                is CellToken -> {
+                    val otherCellText = table.getCellText(token.rowIndex, token.columnIndex)
+                    if (otherCellText.startsWith('=')) {
+                        expressionStack.add(Value(parse(otherCellText.substring(1))))
+                    } else {
+                        expressionStack.add(Value(otherCellText.toDouble()))
                     }
-                    operatorStack.add(token.text)
-                }
-                "—" -> {
-                    operatorStack.add(token.text)
-                }
-                "(" -> operatorStack.add(token.text)
-                ")" -> {
-                    while (operatorStack.lastOrNull() != "(") {
-                        addOperator(operatorStack, expressionStack)
-                    }
-                    operatorStack.safePop()
-                    if (operatorStack.last() in functionTokens) {
-                        addOperator(operatorStack, expressionStack)
-                    }
-                }
-                else -> {
-                    val value = token.text.toDouble()
-                    var sign = 1
-                    while (operatorStack.lastOrNull() == "—") {
-                        sign *= -1
-                        operatorStack.pop()
-                    }
-                    expressionStack.add(Value(sign * value))
                 }
             }
         }
 
         while (operatorStack.isNotEmpty()) {
-            if (operatorStack.last() == "(" || operatorStack.last() == ")") {
+            if (operatorStack.last() == openParenthesis || operatorStack.last() == closeParenthesis) {
                 throw Exception("Mismatched parentheses")
             }
             addOperator(operatorStack, expressionStack)
@@ -165,34 +180,74 @@ class ExpressionParser {
         }
     }
 
-    private fun getPrecedence(operator : String) : Int = when (operator) {
-        "+" -> 2
-        "-" -> 2
-        "*" -> 3
-        "/" -> 3
-        "(" -> 1
-        in functionTokens -> 4
-        else -> throw Exception("Wrong operation type: " + operator)
+    private fun handleOperatorToken(token: OperatorToken, operatorStack: ArrayList<Token>,
+                                    expressionStack: ArrayList<Expression>) {
+        when (token.operation) {
+            '-', '+', '*', '/' -> handleStandardOperationToken(token, operatorStack, expressionStack)
+            '—', '(' -> operatorStack.add(token)
+            ')' -> {
+                while (operatorStack.lastOrNull() != openParenthesis) {
+                    addOperator(operatorStack, expressionStack)
+                }
+                operatorStack.safePop()
+                if (operatorStack.last() is FunctionToken) {
+                    addOperator(operatorStack, expressionStack)
+                }
+            }
+        }
     }
 
-    private fun addOperator(operatorStack : ArrayList<String>, expressionStack : ArrayList<Expression>) {
+    private fun handleStandardOperationToken(token : Token, operatorStack : ArrayList<Token>,
+                                             expressionStack : ArrayList<Expression>) {
+        while (operatorStack.isNotEmpty()
+                && getPrecedence(operatorStack.last()) >= getPrecedence(token)) {
+            addOperator(operatorStack, expressionStack)
+        }
+        operatorStack.add(token)
+    }
+
+    private fun addOperator(operatorStack : ArrayList<Token>, expressionStack : ArrayList<Expression>) {
         val operator = operatorStack.pop()
-        if (operator in functionTokens) {
-            if (expressionStack.size < 1) {
-                throw Exception("Wrong number of arguments")
+        when (operator) {
+            is FunctionToken -> {
+                if (expressionStack.size < 1) {
+                    throw Exception("Wrong number of arguments")
+                }
+                expressionStack.add(Function(operator.text, expressionStack.pop()))
             }
-            expressionStack.add(Function(operator, expressionStack.pop()))
-            return
+            is OperatorToken -> {
+                if (operator == unaryMinus) {
+                    expressionStack.add(Operator(Value(-1.0), '*', expressionStack.pop()))
+                    return
+                }
+                if (expressionStack.size < 2) {
+                    throw Exception("Wrong number of arguments")
+                }
+                val right = expressionStack.pop()
+                val left = expressionStack.pop()
+                expressionStack.add(Operator(left, operator.operation, right))
+            }
         }
-        if (operator == "—") {
-            expressionStack.add(Operator(Value(-1.0), "*", expressionStack.pop()))
-            return
+    }
+
+    private fun handleNumberToken(token : NumberToken, operatorStack : ArrayList<Token>,
+                                  expressionStack : ArrayList<Expression>) {
+        var sign = 1
+        while (operatorStack.lastOrNull() == unaryMinus) {
+            sign *= -1
+            operatorStack.pop()
         }
-        if (expressionStack.size < 2) {
-            throw Exception("Wrong number of arguments")
+        expressionStack.add(Value(sign * token.value))
+    }
+
+    private fun getPrecedence(token : Token) : Int = when (token) {
+        is OperatorToken -> when (token.operation) {
+            '(' -> 1
+            '+', '-' -> 2
+            '*', '/' -> 3
+            else -> throw Exception("Unknown operation type: " + token.operation)
         }
-        val right = expressionStack.pop()
-        val left = expressionStack.pop()
-        expressionStack.add(Operator(left, operator, right))
+        is FunctionToken -> 4
+        else -> throw Exception("Unknown operation type")
     }
 }
